e5c9e983b0ad4caeec90a1bcf0c501673ac1439c
Mert UZAN <mertuzan203@gmail.com>
2019-04-05 18:36:51 +0300

    commit

---
+++ Yazlab2_1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Yazlab2_1
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.openFile = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.mix = new System.Windows.Forms.Button();
+			this.label = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// openFile
+			// 
+			this.openFile.Image = ((System.Drawing.Image)(resources.GetObject("openFile.Image")));
+			this.openFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.openFile.Location = new System.Drawing.Point(12, 9);
+			this.openFile.Name = "openFile";
+			this.openFile.Size = new System.Drawing.Size(100, 26);
+			this.openFile.TabIndex = 0;
+			this.openFile.Text = "Choose image";
+			this.openFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.openFile.UseVisualStyleBackColor = true;
+			this.openFile.Click += new System.EventHandler(this.openFile_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 41);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1218, 772);
+			this.panel1.TabIndex = 1;
+			// 
+			// mix
+			// 
+			this.mix.Image = ((System.Drawing.Image)(resources.GetObject("mix.Image")));
+			this.mix.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.mix.Location = new System.Drawing.Point(118, 9);
+			this.mix.Name = "mix";
+			this.mix.Size = new System.Drawing.Size(84, 26);
+			this.mix.TabIndex = 2;
+			this.mix.Text = "Mix Puzzle";
+			this.mix.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.mix.UseVisualStyleBackColor = true;
+			this.mix.Click += new System.EventHandler(this.mix_Click);
+			// 
+			// label
+			// 
+			this.label.Location = new System.Drawing.Point(937, 9);
+			this.label.Name = "label";
+			this.label.Size = new System.Drawing.Size(268, 26);
+			this.label.TabIndex = 3;
+			this.label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label1
+			// 
+			this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+			this.label1.Location = new System.Drawing.Point(1211, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 26);
+			this.label1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1242, 821);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label);
+			this.Controls.Add(this.mix);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.openFile);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Puzzle";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button openFile;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button mix;
+		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Yazlab2_1/Form1.cs
@@ -0,0 +1,239 @@
+﻿//160202009 MERT HÜSEYİN UZAN
+//160202063 SERKAN YAVAŞ
+//YAZLAB 2 - 1.PROJE PUZZLE
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Yazlab2_1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		OpenFileDialog file = new OpenFileDialog();
+		Bitmap bmp;
+		Bitmap tmp;
+		Button[,] pictures =new Button[4,4];
+		Button[,] real = new Button[4, 4];
+		int score = 100;
+		Boolean scoreflag = false;
+		int isClicked = 0;
+		Button previous;
+		int correct = 0;
+		int highscore=0;
+		String highscoreDate;
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			panel1.Visible = false;
+			String txt = "enyuksekskor.txt";
+			var lines = File.ReadLines(txt);
+			foreach (var line in lines)
+			{
+				String s = line.Substring(17);
+				String d = line.Substring(0, 16);
+				int n = int.Parse(s);
+				if (n > highscore)
+				{
+					highscoreDate = d;
+					highscore = n;
+				}
+			}
+			label.Text = "Best Score: "+highscore+" Date: "+highscoreDate;
+		}
+
+		private void openFile_Click(object sender, EventArgs e)
+		{
+			file.Filter = " PNG |*.png| BMP |*.bmp| JPEG |*.jpeg| JPG |*.jpg";
+			file.FilterIndex = 4;
+			file.RestoreDirectory = true;
+			file.CheckFileExists = false;
+			file.Title = "Select picture";
+			file.ShowDialog();
+			images();
+
+		}
+
+		private void images()
+		{
+			panel1.AutoScroll = true;
+			Image image = Image.FromFile(file.FileName);
+			tmp = (Bitmap)image;
+
+			int h = image.Size.Height;
+			int y = (this.ClientSize.Height / 2) - ((2 * h / 4) + 3);
+			int w = image.Size.Width;
+			int x= (this.ClientSize.Width / 2) - ((2 * w/4) + 3);
+			int imageHeight = image.Size.Height / 4;
+			int imageWidth = image.Size.Width / 4;
+
+			for (int i = 0; i < 4; i++)
+			{
+				for(int j = 0; j < 4; j++)
+				{
+					real[i, j] = new Button();
+					real[i, j].Size = new Size(w / 4, h / 4);
+					String name = "real";
+					name = name + i + "" + j;
+					real[i, j].Name = name;
+					bmp = tmp.Clone(new System.Drawing.Rectangle(j * imageWidth, i * imageHeight, imageWidth, imageHeight), tmp.PixelFormat);
+					real[i, j].Image = bmp;
+
+
+					pictures[i, j] = new Button();
+					pictures[i, j].Size= new Size(w / 4, h / 4);
+					pictures[i, j].Location=new Point(j * (w/4) + x, i * (h/4) + y);
+					String name2 = "picture";
+					name2 = name2 + i+""+j;
+					pictures[i, j].Name = name2;
+					pictures[i, j].Click += pictureClick;
+					panel1.Controls.Add(pictures[i, j]);
+					bmp = tmp.Clone(new System.Drawing.Rectangle(j * imageWidth, i * imageHeight, imageWidth, imageHeight), tmp.PixelFormat);
+					pictures[i, j].Image = bmp;
+					
+				}
+			}
+		}
+	
+		private void control(Button button)
+		{ 
+			int x= int.Parse(""+ button.Name.Substring(7,1));
+			int y= int.Parse(""+ button.Name.Substring(8));
+			int w = real[x, y].Width;
+			int h = real[x, y].Height;
+			Bitmap real_ = (Bitmap)real[x, y].Image;
+			Bitmap clicked = (Bitmap)button.Image;
+			int counter=0;
+			for(int i = 0; i < w; i++)
+			{
+				for(int j = 0; j < h; j++)
+				{
+					Color pixelreal = real_.GetPixel(i, j);
+					Color pixelclicked = clicked.GetPixel(i, j);
+					if (pixelreal.Equals(pixelclicked))
+					{
+						counter++;
+					}
+				}
+			}
+			if (counter == w * h)
+			{
+				button.Click -= pictureClick;
+				correct++;
+				scoreflag = true;
+				Console.WriteLine("Correct puzzle piece:" + correct);
+				if (correct >= 16)
+				{
+					finish();
+				}
+			}
+			else
+			{
+				if (!scoreflag)
+				{
+					score -= 5;
+				}
+			}
+			Console.WriteLine(score);
+		}
+
+		private void pictureClick(object sender, EventArgs e)
+		{
+			mix.Visible = false; // After click any puzzle piece mix&file button gets locked
+			openFile.Visible = false;
+			Button pic = (Button)sender;
+
+			if (isClicked==1)
+			{
+				isClicked = 0;
+				Image tmp = previous.Image;
+				previous.Image = pic.Image;
+				pic.Image = tmp;
+				control(pic);
+				control(previous);
+				scoreflag = false;
+			}
+			else
+			{
+				previous = pic;
+				isClicked=1;
+			}
+		}
+
+		private void finish()
+		{
+			TextWriter textWriter = new StreamWriter("enyuksekskor.txt", true);
+			DateTime dt = DateTime.Now;
+			textWriter.WriteLine(dt.ToString("MM/dd/yyyy HH:mm") + ";" + score);
+			textWriter.Close();
+			Console.WriteLine("Puzzle has done. Score: "+score);
+			DialogResult result = MessageBox.Show("Puzzle has done. Score:" + score, "Congratulations!");
+			if (result == DialogResult.OK)
+			{
+				Environment.Exit(1);
+			}
+		}
+
+		private void mix_Click(object sender, EventArgs e)
+		{
+			panel1.Visible = true;
+			Random r = new Random();
+			
+			for (int i = 0; i < 8; i++)
+			{
+				int random = r.Next(0, 4);
+				int random2 = r.Next(0, 4);
+				int random3 = r.Next(0, 4);
+				int random4 = r.Next(0, 4);
+				Image tmp = pictures[random, random2].Image;
+				pictures[random, random2].Image = pictures[random3, random4].Image;
+				pictures[random3, random4].Image = tmp;
+			}
+			int counter2 = 0;
+			correct = 0;
+
+			//Getting number of correct pictures after the mixing process
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					Bitmap pic_=(Bitmap)pictures[i, j].Image;
+					Bitmap real_= (Bitmap)real[i, j].Image;
+					int w = real_.Width;
+					int h = real_.Height;
+
+					for (int x = 0; x < w; x++)
+					{
+						for (int y = 0; y < h; y++)
+						{
+							if (real_.GetPixel(x, y).Equals(pic_.GetPixel(x, y))) // Comparing pixels
+							{
+								counter2++;
+							}
+
+						}
+					}
+					if (counter2 == w * h)
+					{
+						correct++;
+						pictures[i, j].Click -= pictureClick;
+					}
+					counter2 = 0;
+				}
+			}
+			Console.WriteLine(correct);
+			if (correct >= 16)
+			{
+				finish();
+			}
+		}
+
+		
+	}
+}
+
